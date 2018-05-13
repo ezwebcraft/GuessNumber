5a7eaaff016cbb521f3eb21599997272573bb17c
Elvin Ramirez <real8686@gmail.com>
2018-05-13 18:27:00 -0400

    add init files and start project

---
+++ Guess_Program.py
@@ -0,0 +1,14 @@
+#import random
+import random
+
+print('----------------------------')
+print('   Guess That Number Game   ')
+print('----------------------------')
+print()
+
+
+the_number = random.randint(0, 100)
+
+
+
+
