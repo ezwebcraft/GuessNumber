b78156a2d7fabe1db2f482137dfebd13471fec2c
Elvin Ramirez <real8686@gmail.com>
2018-05-13 20:32:25 -0400

    finish up project

--- Guess_Program.py
+++ Guess_Program.py
@@ -1,4 +1,3 @@
-#import random
 import random
 
 print('----------------------------')
@@ -6,9 +5,28 @@ print('   Guess That Number Game   ')
 print('----------------------------')
 print()
 
-
 the_number = random.randint(0, 100)
 
+guess = -1
+
+name = input('Player, Please your name')
+
+while guess !=  the_number:
+
+    guess_test = input('Guess a number between 0 and 100: ')
+
+    guess = int(guess_test)
+
+    if guess < the_number:
+        print('Hey {} Your Number {} was too Low !!!'.format(name, guess))
+    elif guess > the_number:
+        print('Hey {} Your Number {} was too High !!!'.format(name, guess))
+    else:
+        print('Hey {} you wish'.format(name))
+
+# TODO: remove this line
+# print(guess_test, type(guess_test))
+# print(guess, type(guess))
 
 
-
+print('done')
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# GuessNumber
+### GuessNumber
+
+##### Concept covered
+
+- conditional statements (`if , elif, else`)
+- review `type`
+- review `input` method 
+- review Truthiness 
+- shape of a program 
